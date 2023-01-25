ab5a73321fce4812b0cd5e81d3ce0a5a34200069
shangkegit <weizhe_long@126.com>
2023-01-25 10:43:20 +0800

    优化API_KEY获取逻辑

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ def load_config():
     config_str = read_file(config_path)
     # 将json字符串反序列化为dict类型
     config = json.loads(config_str)
-    config['open_ai_api_key']=os.getenv('OPEN_AI_API_KEY')
+    if config['open_ai_api_key'] is None or config['open_ai_api_key']=='':
+        config['open_ai_api_key']=os.getenv('OPEN_AI_API_KEY')
     logger.info("[INIT] load config: {}".format(config))
 
 
